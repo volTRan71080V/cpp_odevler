e04fbbbc21cda863eee4ad6558509da7d7de0e07
Bahadir Yigit <bahadiryigit0307@gmail.com>
2023-05-08 22:43:40 +0300

    yasir3

---
+++ yasir/odev_3/odev3.cpp
@@ -0,0 +1,61 @@
+
+
+#include <iostream>
+using namespace std;
+#include <unistd.h>
+
+
+void ft_ultimate_mega_extra_super_combo_MAX_XXX_PRO_ft(int *********nbr) {
+    *********nbr = 42;
+}
+
+    void ft_ft(int *nbr)
+    {
+
+       *nbr = 74080;
+
+    }
+
+
+
+
+    void ft_print_comb()
+    {
+        char bosluk =' ';
+        for(char a = 0+48;a<56;a++)
+        {
+
+            for(char b = a+1;b<57;b++)
+            {
+                for(char c = b+1;c<58;c++)
+                {
+
+                    write(1,&a,1);
+                    write(1,&b,1);
+                    write(1,&c,1);
+                    write(1," ",1);
+                }
+            }
+        }
+
+    }
+
+
+    int main()
+    {
+        ft_print_comb();
+        int *ptr;
+        ft_ft(ptr);
+    int sayi = 0;
+    int *ptr1 = &sayi;
+    int **ptr2 = &ptr1;
+    int ***ptr3 = &ptr2;
+    int ****ptr4 = &ptr3;
+    int *****ptr5 = &ptr4;
+    int ******ptr6 = &ptr5;
+    int *******ptr7 = &ptr6;
+    int ********ptr8 = &ptr7;
+    int *********ptr9 = &ptr8;
+    ft_ultimate_mega_extra_super_combo_MAX_XXX_PRO_ft(ptr9);
+        return 0;
+    }
